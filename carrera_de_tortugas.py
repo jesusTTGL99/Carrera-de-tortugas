dddcce903f997eb953403db4a29c260aa269e338
jesusTTGL99 <jegonzaleza.18@est.ucab.edu.ve>
2021-11-12 18:53:42 -0400

    Personalizando estilos del juego

--- carrera_de_tortugas.py
+++ carrera_de_tortugas.py
@@ -3,15 +3,22 @@ import random
 
 s = turtle.Screen()
 s.title("Carrera de tortugas")
+s.bgcolor("gray")
 
 jugador1 = turtle.Turtle()
 jugador2 = turtle.Turtle()
 
+jugador1.hideturtle()
 jugador1.shape("turtle")
 jugador1.color("red", "red")
+jugador1.shapesize(2, 2, 2)
+jugador1.pensize(3)
 
+jugador2.hideturtle()
 jugador2.shape("turtle")
 jugador2.color("blue", "blue")
+jugador2.shapesize(2, 2, 2)
+jugador2.pensize(3)
 
 jugador1.penup()
 jugador1.goto(200, 100)
@@ -21,6 +28,7 @@ jugador1.circle(40)
 jugador1.penup()
 jugador1.goto(-200, 140)
 jugador1.pendown()
+jugador1.showturtle()
 
 jugador2.penup()
 jugador2.goto(200, -100)
@@ -30,5 +38,6 @@ jugador2.circle(40)
 jugador2.penup()
 jugador2.goto(-200, -60)
 jugador2.pendown()
+jugador2.showturtle()
 
 turtle.done()
